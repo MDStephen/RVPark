bc0035bc2bf9d39f0865d57c5ed1b0de55fd3da5
MDStephen <mahlonstephenson@mail.weber.edu>
2026-06-27 20:22:27 -0600

    feat: initializing directory and files

---
+++ asp-net-web-app/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp_net_web_app.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
