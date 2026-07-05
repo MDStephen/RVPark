bfd406d2da0df124bc50e837d84f5794fdf5a403
MDStephen <mahlonstephenson@mail.weber.edu>
2026-07-05 17:28:40 -0600

    feat: defining filter model and filter list blueprint system

--- asp-net-web-app/Models/FilterModel.cs
+++ asp-net-web-app/Models/FilterModel.cs
@@ -1,23 +1,16 @@
-
-/*
 public class FilterModel {
 
     public class FilterDefinition {
-        private string name;
+        private string[] acceptableTypes = ["Dropdown", "StartDate", "EndDate", "Search"];
         private string type;
+        private string name;
+        private list<T> data;
 
-        public FilterDefinition(string name, string type){
-            this.name = name;
-            this.type = type;
+        public FilterDefinition(string type, dbSet<T> table){
+            this.name = //tableName;
+            if (acceptableTypes.contains(type)){this.type = type;}
+            this.data = //tableData;//obviously only works with very small data set which is OK for this assignment
         }
     }
-
-    public class FilterPanelModel {
-        public List<FilterDefinition> Filters { get; set; } = [];
-
-        public 
-    }
-
 }
 
-*/
